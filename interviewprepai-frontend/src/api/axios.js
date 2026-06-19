3d8cadba037a139623982ebafc03aa6baf4eea6e
DeepikaPeddi <deepikapeddi74@gmail.com>
2026-06-19 14:33:35 +0530

    Configure frontend API environment variable

--- interviewprepai-frontend/src/api/axios.js
+++ interviewprepai-frontend/src/api/axios.js
@@ -2,12 +2,11 @@ import axios from "axios";
 
 const api = axios.create({
 
-    baseURL: "http://localhost:8080/api"
+    baseURL: import.meta.env.VITE_API_URL
 
 });
 
 api.interceptors.request.use(
-
     (config) => {
 
         const token =
@@ -17,13 +16,10 @@ api.interceptors.request.use(
 
             config.headers.Authorization =
                 `Bearer ${token}`;
-
         }
 
         return config;
-
     }
-
 );
 
 export default api;
